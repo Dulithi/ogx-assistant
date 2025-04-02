a0af42dc46513729e4b3a4808e3280752fa64477
dulithij <dulithij@boardpac.org>
2025-04-02 17:15:05 +0530

    minor changes

--- app/page.tsx
+++ app/page.tsx
@@ -22,37 +22,37 @@ const Home = () => {
     }
 
     return (
-        <main className="flex flex-col h-screen p-4 bg-gray-100">
+        <main className="flex flex-col h-screen p-4 bg-primary">
              <h1 className=" text-center font-extrabold text-5xl m-6"><span className="bg-clip-text text-transparent bg-gradient-to-r to-gv from-gta via-gte">oGX Assistant</span></h1>
             
             <section className="flex-grow flex flex-col justify-end overflow-hidden">
                 {noMessages ? (
                     <div className='flex flex-col items-center text-center p-6'>
-                        <p className="text-gray-500 text-lg">Clarify your doubts about the AIESEC Exchange program!</p>
+                        <p className="text-primary-content text-lg">Clarify your doubts about the AIESEC Exchange program!</p>
                         <br />
                         <PromptSuggestionsRow onPromptClick={handlePrompt} />
                     </div>
                 ) : (
-                    <div className='overflow-y-auto flex flex-col p-4 space-y-2 max-h-[65vh] bg-white rounded-xl shadow-md'>
+                    <div className='overflow-y-auto flex flex-col p-4 space-y-2 max-h-[65vh] rounded-xl'>
                         {messages.map((message) => (
                             <ChatBubble key={message.id} message={message} />
                         ))}
-                        {status !== 'ready' && <div className='text-left'><span className="loading loading-dots loading-md"></span></div>}
+                        {status !== 'ready' && <div className='text-left'><span className="loading loading-dots loading-md text-aiesec-blue"></span></div>}
                     </div>
                 )}
             </section>
             
-            <div className='p-4 bg-white shadow-md rounded-xl'>
+            <div className='p-4 rounded-xl'>
                 <form className="flex items-center space-x-3" onSubmit={handleSubmit}>
                     <input 
-                        className="flex-grow p-3 border border-gray-300 rounded-full focus:ring-2 focus:ring-blue-500 outline-none"
+                        className="text-primary-content flex-grow p-3 border border-gray-300 rounded-full focus:ring-2 focus:ring-aiesec-blue outline-none"
                         type="text" 
                         value={input} 
                         onChange={handleInputChange} 
                         placeholder="Ask me something..."
                         disabled={status !== 'ready'}
                     />
-                    <button type="submit" className="bg-blue-500 text-white p-3 rounded-full hover:bg-blue-600 transition duration-200">
+                    <button type="submit" className="bg-aiesec-blue text-white p-3 rounded-full hover:bg-aiesec-blue transition duration-200">
                         <SendHorizontal size={20} />
                     </button>
                 </form>
